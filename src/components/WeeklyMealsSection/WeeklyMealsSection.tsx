2cec1c022da1e84c217a001aea6eb603fa34406b
Wyatt <wyattchannings@gmail.com>
2026-07-19 08:26:13 +0800

    Add Modal trigger styles prop

--- src/components/WeeklyMealsSection/WeeklyMealsSection.tsx
+++ src/components/WeeklyMealsSection/WeeklyMealsSection.tsx
@@ -66,6 +66,7 @@ export const WeeklyMealsSection = () => {
                           ))}
                         </ul>
                       }
+                      triggerStyles="text-start"
                     />
                   ) : (
                     <>
